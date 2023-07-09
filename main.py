7a96b0230ee264c0a2c52f6d62351172b1c24b65
Kenneth Leung <kennethleung.ty@gmail.com>
2023-07-09 12:04:10 +0800

    Update config and output prints

--- main.py
+++ main.py
@@ -27,7 +27,16 @@ if __name__ == "__main__":
     response = dbqa({'query': args.input})
     end = timeit.default_timer()
 
-    print(response['result'])
+    print(f'\nAnswer: {response["result"]}')
     print('='*50)
-    print(response['source_documents'])
+
+    # Process source documents
+    source_docs = response['source_documents']
+    for i, doc in enumerate(source_docs):
+        print(f'\nSource Document {i+1}\n')
+        print(f'Source Text: {doc.page_content}')
+        print(f'Document Name: {doc.metadata["source"]}')
+        print(f'Page Number: {doc.metadata["page"]}\n')
+        print('='* 60)
+
     print(f"Time to retrieve response: {end - start}")
